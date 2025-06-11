6387b273663ef020b38a50a00aadc7a7dbea016d
roypushpak <roypushpak3@gmail.com>
2025-06-11 14:30:50 -0400

    Enable error reporting in index.php

--- reminders-app/index.php
+++ reminders-app/index.php
@@ -1,6 +1,18 @@
 <?php 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 require_once 'app/init.php';
 
-$app = new App;
+try {
+    $app = new App;
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    if (isset($app) && $app->db) {
+        echo '<br>DB Connection: Active';
+    } else {
+        echo '<br>DB Connection: Failed';
+    }
+}
 ?>
